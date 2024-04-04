1880e4cf2ea8c0b31fd43acbdcabaaf202ee6df1
neonman019 <165931374+neonman019@users.noreply.github.com>
2024-04-05 00:38:57 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,18 +4,15 @@ header("Content-Type: application/vnd.apple.mpegurl");
 $d0 = isset($_GET["id"]) ? $_GET["id"] : "";
 $j1 = isset($_GET["au"]) ? $_GET["au"] : "";
 
-$n2 = "http://mob.satocean.co/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/{$d0}&series=&forced_storage=0&disable_ad=0&download=0&force_ch_link_check=0&JsHttpRequest=1-xml";
-
-
-
+$n2 = "http://dhoom4u.me/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http://localhost/ch/{$d0}&series=&forced_storage=0&disable_ad=0&download=0&force_ch_link_check=0&JsHttpRequest=1-xml";
 
 $t4 = array(
-    "Cookie: mac=00:1A:79:A6:77:4A",
-    "Authorization: Bearer  3C8E0560EC24A93B9EAAA82F0CFF5037",
-    "Referer: http://mob.satocean.co/stalker_portal/c/",
+    "Cookie: mac=00:1a:79:a7:e4:26",
+    "Authorization: Bearer DCFE189072DA725604FCCB5AFCB94428",
+    "Referer: http://dhoom4u.me/stalker_portal/c/",
     "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
     "X-User-Agent: Model: MAG250; Link:",
-    "Referer: http://mob.satocean.co/stalker_portal/c/"
+    "Referer: http://dhoom4u.me/stalker_portal/c/"
 );
 
 $n3 = curl_init();
@@ -25,7 +22,7 @@ curl_setopt($n3, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($n3, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($n3, CURLOPT_HTTPHEADER, $t4);
 curl_setopt($n3, CURLOPT_USERAGENT, 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3');
-curl_setopt($n3, CURLOPT_REFERER, 'http://mob.satocean.co/stalker_portal/c/');
+curl_setopt($n3, CURLOPT_REFERER, 'http://dhoom4u.me/stalker_portal/c/');
 
 $h5 = curl_exec($n3);
 
@@ -35,7 +32,7 @@ $i6 = json_decode($h5, true);
 
 if (isset($i6["js"]["cmd"])) {
     $d7 = $i6["js"]["cmd"];
-    header("Location:  " . $d7);
+    header("Location: " . $d7);
     die;
 }
 ?>
